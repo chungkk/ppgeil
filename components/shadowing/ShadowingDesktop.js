da0703fb5f2f544ab948e1ebfe9961d5bc9d3d9f
Chungkk <hoatiuthu@gmail.com>
2025-11-29 16:53:18 +0100

    giao diện desktio shadow nút dịch

--- components/shadowing/ShadowingDesktop.js
+++ components/shadowing/ShadowingDesktop.js
@@ -330,15 +330,10 @@ const ShadowingDesktop = ({
                     onClick={() => setShowTranslation(!showTranslation)}
                     title={showTranslation ? 'Ẩn bản dịch' : 'Hiện bản dịch'}
                   >
-                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                      <path d="M5 8l6 6"/>
-                      <path d="M4 14l6-6 2-3"/>
-                      <path d="M2 5h12"/>
-                      <path d="M7 2h1"/>
-                      <path d="M22 22l-5-10-5 10"/>
-                      <path d="M14 18h6"/>
-                    </svg>
-                    <span>Dịch</span>
+                    <span className={styles.toggleLabel}>Dịch</span>
+                    <div className={styles.toggleSwitch}>
+                      <div className={styles.toggleSlider}></div>
+                    </div>
                   </button>
                 </div>
                 <div style={{ pointerEvents: 'none' }}>
