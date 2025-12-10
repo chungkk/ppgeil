3af4c987b1ea998f460a8a361ff730c5b5cc1ffe
Chungkk <hoatiuthu@gmail.com>
2025-12-10 21:29:14 +0100

    fix

--- components/LessonCard.js
+++ components/LessonCard.js
@@ -99,21 +99,6 @@ const LessonCard = ({ lesson, onClick }) => {
 
       <div className={styles.cardContent}>
         <h3 className={styles.title}>{lesson.title}</h3>
-
-        <div className={styles.actions}>
-          <button
-            className={styles.actionBtn}
-            onClick={(e) => {
-              e.stopPropagation();
-              if (lesson.onDictationClick) lesson.onDictationClick();
-            }}
-          >
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-            </svg>
-            <span>{t('lessonCard.dictation')}</span>
-          </button>
-        </div>
       </div>
     </div>
   );
