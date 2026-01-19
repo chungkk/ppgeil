0b7e373cbf600238f57e2798b030b81aaa7a75cc
Chungkk <hoatiuthu@gmail.com>
2026-01-19 11:11:47 +0100

    admin retro

--- pages/_document.js
+++ pages/_document.js
@@ -10,14 +10,22 @@ export default function Document() {
           href="/styles/DictionaryPopup.module.css"
           as="style"
         />
-        
+
+        {/* Google Fonts for Retro Admin Theme */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700&family=Press+Start+2P&family=VT323&display=swap"
+          rel="stylesheet"
+        />
+
         {/* DNS Prefetch for external resources */}
         <link rel="dns-prefetch" href="https://api.openai.com" />
         <link rel="dns-prefetch" href="https://api.groq.com" />
-        
+
         {/* Preconnect to speed up API calls */}
         <link rel="preconnect" href="https://api.openai.com" crossOrigin="anonymous" />
-        
+
         {/* Resource hints for better performance */}
         <link rel="prefetch" href="/api/dictionary" />
         <link rel="prefetch" href="/api/translate" />
